0513c6d712c8316a663e03739ee85c7577cae81e
tYohannes <teklegy@gmail.com>
2022-04-04 21:57:19 +0300

    Refactor code to use accessor method for nameable attribute

--- capitalize_name_decorator.rb
+++ capitalize_name_decorator.rb
@@ -3,6 +3,6 @@ require_relative './name_decorator'
 # class CapitalizeName decorator
 class CapitalizeName < NameDecorator
   def correct_name
-    @nameable.correct_name.capitalize
+    nameable.correct_name.capitalize
   end
 end
--- trim_name_decorator.rb
+++ trim_name_decorator.rb
@@ -3,6 +3,6 @@ require_relative './name_decorator'
 # TrimmerDecorator class
 class TrimmerDecorator < NameDecorator
   def correct_name
-    @nameable.correct_name[0, 10]
+    nameable.correct_name[0, 10]
   end
 end
